801d018ac49065edc666630beb9753e9e966d442
Scott Fahrig <scottfahrig@Scotts-MBP.attlocal.net>
2021-01-06 11:52:10 -0600

    JS displaying arrays in browser console but not in markup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,46 @@
 // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyCMZj5I4ynCYX9rijrDdWSxrlpLnaK0sAI",
-    authDomain: "single-show-calendar.firebaseapp.com",
-    databaseURL: "https://single-show-calendar-default-rtdb.firebaseio.com",
-    projectId: "single-show-calendar",
-    storageBucket: "single-show-calendar.appspot.com",
-    messagingSenderId: "554986401392",
-    appId: "1:554986401392:web:bd2c514b417ec45d260eb5",
-    measurementId: "G-3Z5S8G8021"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  const Http = new XMLHttpRequest();
-  const url='https://single-show-calendar-default-rtdb.firebaseio.com/posts.json';
-  Http.open("GET", url);
-  Http.send();
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCMZj5I4ynCYX9rijrDdWSxrlpLnaK0sAI",
+  authDomain: "single-show-calendar.firebaseapp.com",
+  databaseURL: "https://single-show-calendar-default-rtdb.firebaseio.com",
+  projectId: "single-show-calendar",
+  storageBucket: "single-show-calendar.appspot.com",
+  messagingSenderId: "554986401392",
+  appId: "1:554986401392:web:bd2c514b417ec45d260eb5",
+  measurementId: "G-3Z5S8G8021"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.database();
 
-  Http.onreadystatechange = (e) => {
-    var data = (Http.responseText)
-    document.getElementById("demo").innerHTML = JSON.stringify(data, undefined, 2);
-  }
+// Function to convert Firebase snapshot to JS array
+function snapshotToArray(snapshot) {
+    var returnArr = [];
+
+    snapshot.forEach(function(childSnapshot) {
+        var posts = childSnapshot.val();
+        posts.key = childSnapshot.key;
+        var x = childSnapshot.toString();
+        document.getElementById("demo").innerHTML = x;
+
+        returnArr.push(posts);
+
+        
+    });
+
+    return returnArr;
+
+    for (var [i] = 0; i < posts.length; i++) {
+        var name = posts[i];
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(item));
+        ul.appendChild(li);
+    }
+};
+
+firebase.database().ref('/posts').on('value', function(snapshot) {
+    console.log(snapshotToArray(snapshot));
+
+});
